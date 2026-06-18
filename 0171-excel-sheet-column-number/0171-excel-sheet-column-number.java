993350896965b9406efca399124a18601c1535d3
bharathraj <155177856+Bharat8856@users.noreply.github.com>
2026-06-18 14:11:11 +0530

    Time: 1 ms (91.62%), Space: 43.4 MB (71.64%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int titleToNumber(String s) {
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            ans=ans*26+(ch -'A'+1);
+        }
+        return (int) ans;
+        
+    }
+}
