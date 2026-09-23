dbf09ddb33502cd364acee91a47e649e5ed2d152
bharathraj <155177856+Bharat8856@users.noreply.github.com>
2026-09-23 12:07:03 +0530

    Time: 6 ms (86.81%), Space: 44 MB (60.85%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] count1=new int[26];
+        int[] count2=new int[26];
+
+        int k =s1.length();
+
+        if(s1.length() > s2.length()) {
+        return false;
+        }
+
+        for(char c:s1.toCharArray()){
+            count1[c-'a']++;
+
+        }
+
+        for(int i=0;i<k;i++){
+            count2[s2.charAt(i)-'a']++;
+        }
+
+        if(Arrays.equals(count1,count2)){
+            return true;
+        }
+
+        for(int i=k;i<s2.length();i++){
+            count2[s2.charAt(i-k)-'a']--;
+            count2[s2.charAt(i)-'a']++;
+            if(Arrays.equals(count1,count2)){
+            return true;
+        }
+        }
+       return false;
+    }
+}
